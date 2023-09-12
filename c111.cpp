506f6dcaa0b75993073b120648e59cb7a0b46d67
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-09-12 17:53:13 +0900

    Add files via upload

---
+++ c111.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+ printf("s");
+ return 0;
+
---
+++ c22.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main( )
+{
+
+   printf("now %dmonth %ddays",9,12);
+   printf("Now %dhours %dminute \n",5,45);
+   printf("that's %d cm \n",12 );
+   printf("%lf km \n",12.378);
+   printf("%dg \n", 34536);
+   printf("34.5% is %d, %d, %d \n",3,4,5);
+}
