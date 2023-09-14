50f8af1312261c604e11b93f8d41594189bcd12c
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-09-14 17:40:05 +0900

    Add files via upload

---
+++ cats2.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("\\    /\\ \n");
+	printf(" )  (  ') \n");
+	printf("(  /   ) \n");
+	printf(" \\ (___) | \n");
+	
+	return 0;
+}
+//{
+//printf("%%Solar Tracking System%% \n");
+//printf("\\smart Devices\\ \n");
+//printf("Matrix \n");
+//printf("Hello Robot \n");
+
+//return 0;
+//}
