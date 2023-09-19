42fcc17684adccac47ad042c0a6deeb5aa81b765
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-09-19 17:55:44 +0900

    Add files via upload

---
+++ c1-11(2).cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main()
+/*{
+	int m;
+    double km;
+	
+	printf("반환받을 값을 입력하세요.");
+	scanf("%d", &m);
+	
+	km = m/1000.0;
+	 
+	
+	printf("%dm, %.1lfkm.",m,km);
+	
+}
+*/
+/*{
+	int n;
+	int m;
+	
+	printf("what your favorite number?");
+	scanf("%d",&n);
+	scanf("%d",&m);
+	printf("%d,%d is your favorite number.",n,m);
+	
+}
+*/
+{
+	double gong;
+	double zu;
+	
+    printf("hey");
+    scanf("%lf",&gong);
+    scanf("%lf",&zu);
+    printf("%lf,%lf babo",gong,zu);
+}
