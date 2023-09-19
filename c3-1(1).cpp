637019b63e765fb7e5a2703f3da04cb2324dd5ce
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-09-19 17:13:49 +0900

    Add files via upload

---
+++ c3-1(1).cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+/*
+{	
+	int a;
+	double f1;
+	char ch;
+	
+	a = 15;
+	f1 = 123.456;
+	ch = 'A';
+	
+	printf("%d\n", a);
+	printf("%f\n", f1);
+	printf("%c\n", ch);
+
+    	
+      
+}
+*/
+/*int main()
+{
+	int a= 10;
+	printf("%d\n", a);
+	
+	a = 20;
+	printf("%d\n", a);
+	
+	return 0;
+}
+
+	int main()
+	{
+		int num;
+		
+		printf("please write a number. :");
+		scanf("%d",&num);
+		printf("%d\n", num);
+		
+		return 0;
+	}
+	*/
+	{
+		double key;
+		double blood;
+		
+		
+		printf("please write your stature and blood. : ");
+		scanf("%lf %c" ,&key , &blood);
+		printf(" cm : %.1lf\n, blood : %c\n", key, blood);
+		
+		return 0; 
+	}
+
+
