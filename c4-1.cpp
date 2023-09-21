170eb748f20d47d6224ec93253c8d515e4150cab
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-09-21 17:25:38 +0900

    Add files via upload

---
+++ c4-1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+	char wr;
+	
+	printf("My grade is 6th sol class 9.");
+	printf("2023y september 21 \n");
+	printf("sound is degree 20'c, 1s %.1lf m \n",343.5 );
+
+	printf("please write your favorite Alphabet \n :");
+	scanf("%c \n",&num);
+	printf("%c \n", num);
+	
+	/*
+	{
+		printf("please write your favorite word");
+		scanf("%c \n",&wr);
+		printf("%s \n", wr);
+	}
+	*/
+	
+ return 0;		
+}
