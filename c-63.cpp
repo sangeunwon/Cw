42ca6c9a710ab45488880613b3c6c503fb1b4053
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-10-19 18:05:37 +0900

    Add files via upload

---
+++ c-63.cpp
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/*int a;
+	a = 100;
+	
+	a *= 30;
+	printf("a = %d\n", a);
+	a /= 4;
+	printf("a = %d\n", a);
+	a -= 10;
+	printf("a = %d\n", a);
+	
+	int b;
+	b= 50;
+	
+	b +=10; 
+	printf("b = %d\n", b);
+	b *= 10;
+	printf("b = %d\n", b);
+	b -= 10;
+	printf("b = %d\n", b);
+	b /= 2;
+	printf("b = %d\n", b);
+	
+	return 0;
+	*/
+	
+	int n = 0;
+	
+	printf("%d\n", n);
+	printf("%d\n", n += 3);
+	printf("%d\n", n *= 0);
+	printf("%d\n", n += 5);
+	printf("%d\n", n++);
+	printf("%d\n", ++n);
+	printf("%d\n", n % 5);
+	printf("%d\n", --n);
+	printf("%d\n", n--);
+	printf("%d\n", n);
+	
+	return 0;	
+}
