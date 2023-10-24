b2f3a7f6e2feaedf40c28977a58badef87632554
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-10-24 17:46:11 +0900

    Add files via upload

---
+++ c-64.cpp
@@ -0,0 +1,242 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+/*{
+	if(1)
+	{
+		printf("True\tÂü\n");
+		printf("yes\t¸Â¾Æ\n");
+	}
+	else
+	{
+		printf("False\t°ÅÁþ\n");
+		printf("no\t no\n");
+	}
+}
+{
+	int a, b, c;
+	
+	a = 12;
+	b = 56;
+	c = a > b;
+	
+	
+	if(c)
+	{
+		printf("%d is more than %d.\n", a, b);
+	}
+	else
+	{
+		printf("%d is not more than %d\n", a, b);
+	}
+}
+
+{
+	srand(time(NULL));
+	
+	int rnd = rand() % 10240; //random f
+	
+	if (rnd > 500) //code
+	{
+		printf("doing 1\n");
+	}	
+	if (rnd > 750)
+	{
+		printf("doing 2\n");
+	}
+	if (rnd > 900)
+	{
+		printf("doing 3\n");
+	}
+	if (rnd > 1000)
+	{
+		printf("doing 4\n");
+	}
+}
+*/
+{
+	int num;
+	
+	printf("no more then 10000 : ");
+	scanf("%d", &num);
+	
+	 if(num < 0)
+	{
+
+	}
+	/*
+	else if(num < 100)
+	{
+		printf("2\n");
+	}
+	else if(num < 1000)
+	{
+		printf("3\n");
+	}
+	else if(num < 10000)
+	{
+		printf("4\n");
+	}
+		
+}
+*/
+/*{
+	if(1)
+	{
+		printf("True\tÂü\n");
+		printf("yes\t¸Â¾Æ\n");
+	}
+	else
+	{
+		printf("False\t°ÅÁþ\n");
+		printf("no\t no\n");
+	}
+}
+{
+	int a, b, c;
+	
+	a = 12;
+	b = 56;
+	c = a > b;
+	
+	
+	if(c)
+	{
+		printf("%d is more than %d.\n", a, b);
+	}
+	else
+	{
+		printf("%d is not more than %d\n", a, b);
+	}
+}
+
+{
+	srand(time(NULL));
+	
+	int rnd = rand() % 10240; //random f
+	
+	if (rnd > 500) //code
+	{
+		printf("doing 1\n");
+	}	
+	if (rnd > 750)
+	{
+		printf("doing 2\n");
+	}
+	if (rnd > 900)
+	{
+		printf("doing 3\n");
+	}
+	if (rnd > 1000)
+	{
+		printf("doing 4\n");
+	}
+}
+*/
+{
+	int num;
+	
+	printf("no more then 10000 : ");
+	scanf("%d", &num);
+	
+	 if(num < 0)
+	{
+
+	}
+	/*
+	else if(num < 100)
+	{
+		printf("2\n");
+	}
+	else if(num < 1000)
+	{
+		printf("3\n");
+	}
+	else if(num < 10000)
+	{
+		printf("4\n");
+	}
+		
+}
+*/
+/*{
+	if(1)
+	{
+		printf("True\tÂü\n");
+		printf("yes\t¸Â¾Æ\n");
+	}
+	else
+	{
+		printf("False\t°ÅÁþ\n");
+		printf("no\t no\n");
+	}
+}
+{
+	int a, b, c;
+	
+	a = 12;
+	b = 56;
+	c = a > b;
+	
+	
+	if(c)
+	{
+		printf("%d is more than %d.\n", a, b);
+	}
+	else
+	{
+		printf("%d is not more than %d\n", a, b);
+	}
+}
+
+{
+	srand(time(NULL));
+	
+	int rnd = rand() % 10240; //random f
+	
+	if (rnd > 500) //code
+	{
+		printf("doing 1\n");
+	}	
+	if (rnd > 750)
+	{
+		printf("doing 2\n");
+	}
+	if (rnd > 900)
+	{
+		printf("doing 3\n");
+	}
+	if (rnd > 1000)
+	{
+		printf("doing 4\n");
+	}
+}
+*/
+{
+	int num;
+	
+	printf("no more then 10000 : ");
+	scanf("%d", &num);
+	
+	 if(num < 0)
+	{
+	
+	}
+	/*
+	else if(num < 100)
+	{
+		printf("2\n");
+	}
+	else if(num < 1000)
+	{
+		printf("3\n");
+	}
+	else if(num < 10000)
+	{
+		printf("4\n");
+	}
+		
+}
+*/
