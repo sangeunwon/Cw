8daeb455f2e4b1e5914210e5997d9fccf20e7ada
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-11-07 17:34:26 +0900

    Add files via upload

---
+++ c- 65.cpp
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+
+	{
+		int sum;
+		int j;
+		
+		scanf("%d\n", &sum);
+		for (int j = 1; j < 10; j++){
+			
+			if(j % 3 == 0)
+			printf("%d", sum);
+		}
+	}
+	/*
+	int n;
+	
+	for (n=-10; n <= -1; n++)
+	{
+		printf("n = %d\n", n);
+	}
+	
+	printf("Final n = %d\n", n);
+	
+
+	int i;
+	/*
+	
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	
+	printf("Final i = %d\n", i);
+/
+	{
+	 
+	int i;
+	
+	
+	for(i=0; i<10; i++)
+	
+	printf("%d\n", i % 2);
+	
+	
+	
+	int u;
+	int d;
+	/*
+	{
+			int a;	
+	for(a=0; a<10; a++)
+		
+	a = a * -1;
+	printf("%d\n", a);
+				
+	for(i=0; i<10; i++){
+	u = i + 1 % 2;
+	
+	d =  d* -1;		
+	printf("%d\n", u * d);
+			
+	{
+		
+			int z;
+			int l;
+			int y;
+			
+			y = 0;
+			scanf("%d", &z);
+			{
+				for(l = 1; l <=5; l++)
+			
+			y = y + 1;
+
+	printf("%d\n", y);
+	}
+	
+	return 0 ;
+}
+
