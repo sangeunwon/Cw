72fac97b9aa1202cc205a6693ef4476588880f6f
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2023-12-05 17:51:31 +0900

    Add files via upload

---
+++ c - 70.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+
+int main ()
+{
+	/*double height[4];
+	int j = 2;
+	
+	height[0] = 145.7;
+	height[1] = 156.3;
+	height[j] = 147.89;
+	j++;
+	height[j] = 159.4;
+	return 0;
+
+	int ar[10] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	int br[10];
+	
+	int i = 0;
+	
+	for (i = 0; i < 10; i++ ){
+		printf("%d\n", ar[i]);
+		
+		
+	}
+
+	int num[] = {5,2,6,7,8,9,4,3,1};
+	int page[10] = {123, 57, 67, 86};
+	int i,j;
+	
+	for (i = 0; i < 10; i++ ){
+	printf("%d\n", page[i]);
+}
+
+char c; int i; long l; float f; double d;
+
+printf("%d %d %d %d %d\n", sizeof(c), sizeof(i),sizeof(l),sizeof(f), sizeof(d));
+printf("%d %d %d\n", sizeof(char), sizeof(int), sizeof(long));
+printf("%d %d\n", sizeof(float), sizeof(double));
+printf("%d %d\n", sizeof(153), sizeof(5,2));
+return 0 ;
+
+
+int ar[10];
+int br[ ] = {1,2,3,4,5};
+double dr[20];
+
+printf("%d %d %d\n", sizeof(ar), sizeof(br), sizeof(dr));
+*/
+int ar[10] = {13,64,21,42,51,98,12,100,95};
+
+for()
+printf("%d\n",  );
+printf("%d\n");
+}
