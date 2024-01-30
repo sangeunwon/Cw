3c17d005c08f970208658bff46cf72278e63eb26
sangeunwon <144774591+sangeunwon@users.noreply.github.com>
2024-01-30 18:01:54 +0900

    Add files via upload

---
+++ c-73.cpp
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+/*
+	double n;
+	
+	scanf("%lf", &n);
+	printf("%lf", n);
+	
+	return 0;
+	
+	int a,b,c;
+	
+	scanf("%d", &a);
+	printf("a = %d\n", a);
+	scanf("%d%d", &a, &b);
+	printf("a = %d b = %d\n", a, b);
+	scanf("%d%d%d", &a, &b, &c);
+	printf("a = %d b = %d c = %d\n", a,b, c);
+	
+	
+	char ch;
+	
+	scanf("%c", &ch);
+	printf("%c %d", ch, ch);
+	
+	int a;
+	char bn, c;
+	
+	scanf("%d", &a);
+	c = getchar();
+	scanf("%c", &bn);
+	printf("%d %c %d",a, bn, c);
+	*
+	char str[20];
+	
+	scanf("%s", str);
+	printf("%s", str);
+	
+	
+	char str1[15], str2[15];
+	
+	scanf("%s", str1);
+	printf("str1 = %s\n", str1);
+	scanf("%s%s", str1, str2);
+	printf("str1 = %s\n", str1);
+	printf("str2 = %s\n", str2);
+	
+	char str[80];
+	
+	gets(str);
+	printf("%s\n", str);
+	puts(str);
+	
+	char ch;
+	
+	ch = _getche();
+	printf("\nch = %d : %c\n, ch, ch");
+	putchar(ch);
+	
+	int n = 0;
+	
+	while (1)
+	{
+		printf("%d%c", n, 32);
+		n++;
+	}
+	
+	*/
+	char ch;
+	
+	while(1) {
+		scanf("%c", &ch);
+		if (ch== '\n')
+		break;
+		
+		printf("%c", ch);
+	}
+	
+}
+
+
